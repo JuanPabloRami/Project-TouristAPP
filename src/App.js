232bc4cf70352827ba23ac25cc9a4110735808fe
AlejandroMoncada <alejandrolopezmoncada10@gmail.com>
2022-08-08 20:10:59 -0500

    Se realizo la nav con sus perspectivas anclas a los componentes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import { Navbar} from '../src/components/Navbar/Navbar'
-import {Registro} from '../src/components/Registro/Registro'
 import {Login} from '../src/components/Login/Login'
-
+import { Registro } from './components/Registro/Registro.jsx'
+import { NotFound } from './components/Not Found/NotFound.jsx'
 
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar /> 
-      <Registro/>
-      <Login/>
+      <Navbar/>
+      <Routes>
+        <Route path='/register' element={<Registro/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='*' element={<NotFound/>} />
+      </Routes>
+      
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,40 @@
- import React from 'react'
-import { Registro } from '../Registro/Registro'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
+import Logo from '../images/logo.webp'
+import Lupa from '../images/lupa.png'
+import Ubicacion from '../images/ubicacion.png'
  
  export const Navbar = () => {
    return (
      <nav>
-       <p>a</p>
+      <div className="first">
+        <div className="icon">
+          <img className="logo" src={Logo} />
+          <h1>TouristApp</h1>
+        </div>
+        <div className="search-places">
+          <input className='input-search' type='text' placeholder='¿Que deseas buscar?' />
+          <button className='btn-search' type='submit'><img className='img-btn-search' src={Lupa}/></button>
+        </div>
+        <ul className='dropdown-menu'>
+          <li>
+            <p className='accounts'>¿Ya tienes cuenta?</p>
+            <ul className='vertical-menu'>
+              <li><Link to='/login'>Inicia sesión</Link></li> 
+              <li><Link to='/register'>Registrate</Link></li> 
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div className="second">
+        <img src={Ubicacion}/>
+        <h3>Ubicación: Armenia - Quindío</h3>
+      </div>
+      <div className="third">
+        <p>Acerca de</p>
+        <p>Ayuda</p>
+      </div>
      </nav>
    )
  }
