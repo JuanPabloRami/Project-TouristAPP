bc1e64889c7f5b072f3ff04c0c34f97652c61764
alejandrolotero1 <alejandrolotero004@gmail.com>
2022-08-17 22:45:42 -0500

    Se realizaron ajustes en el footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { FaBeer } from 'react-icons/fa';
+import { FaFacebook as IconFacebook, FaYoutube as IconYoutube} from 'react-icons/fa';
+import {BsTwitter as IconTwitter, BsGithub as IconGithub, BsGithub} from 'react-icons/bs';
+
 
 
 export const Footer = () => {
@@ -10,19 +14,33 @@ export const Footer = () => {
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore temporibus beatae aut voluptates dolor iure, quam ad voluptas? Magni nostrum quam voluptatum id aliquam vitae eos assumenda necessitatibus perspiciatis aspernatur!</p>
                 </div>
                 <div className='contentButtons'>
-                    <div className='aboutUss'>
-                        <h3>Sobre Nosotros</h3>
-                        <p><a href="/">Sobre Nosotros</a></p>
-                        <p><a href="/">Terminos y Condiciones</a></p>
-                        <p><a href="/">Politica de privacidad</a></p>
+                    <div className='contentOptions'>
+                        <div className='aboutUss'>
+                            <h3>Sobre Nosotros</h3>
+                            <p><a href="/">Sobre Nosotros</a></p>
+                            <p><a href="/">Terminos y Condiciones</a></p>
+                            <p><a href="/">Politica de privacidad</a></p>
+                        </div>
+                        <div className='customerService'>
+                            <h3>Servicio al cliente</h3>
+                            <p><a href="/">Centro de ayuda</a></p>
+                            <p><a href="/">Terminos y Condiciones</a></p>
+                            <p><a href="/">Politica de privacidad</a></p>
+                        </div>
                     </div>
-                    <div className='customerService'>
-                        <h3>Servicio al cliente</h3>
-                        <p><a href="/">Centro de ayuda</a></p>
-                        <p><a href="/">Terminos y Condiciones</a></p>
-                        <p><a href="/">Politica de privacidad</a></p>
+                    <div className='contentInfoRed'>
+                        <hr />
+                        <div className='icons'>
+                            <a href='/' className="iconBtn"><IconFacebook /></a>
+                            <a href='/' className="iconBtn"><IconTwitter/></a>
+                            <a href='/' className="iconBtn"><IconYoutube/></a>
+                            <a href='/' className="iconBtn"><BsGithub/></a>
+                        </div>
                     </div>
                 </div>
+                
+                
+
             </div>
             <div className='subFooter'>
                 <h4>© Tourist App, 2022. <br />Dev Group, Armenia - Quindio - Teléfono: (57) 3012117531</h4>
