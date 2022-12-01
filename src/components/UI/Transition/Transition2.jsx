de5d200f3164cf9c532d6bbcfcfcc415e87263ab
JuanPabloRami <juanpalabloramirezavs@gmail.com>
2022-11-30 23:32:46 -0500

    finalizados componentes de carga home.

--- src/components/UI/Transition/Transition2.jsx
+++ src/components/UI/Transition/Transition2.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import {ScaleLoader} from 'react-spinners'
+import Transition2Style from './Transition2.module.css'
 
 export const Transition2 = () => {
   return (
-    <div className="BusinessLoading">
+    <>
+    <div className={Transition2Style.ScreenLoading}>
       <div>
         <ScaleLoader
+        color="#ffffff"
           height={60}
           width={10}
         />
         <h3>Cargando</h3>
       </div>
     </div>
+    </>
   )
 }
