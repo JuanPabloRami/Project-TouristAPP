495490848779d485fd9fa80290f2622b3c1ca0b4
AlejandroMoncada <alejandrolopezmoncada10@gmail.com>
2022-12-06 08:04:49 -0500

    Se hizo el css del select cambiar idiomas

--- src/components/UI/SelectLanguage/SelectLanguage.jsx
+++ src/components/UI/SelectLanguage/SelectLanguage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './SelectLanguage.css'
 //traducciones
 import { useTranslation } from "react-i18next"
 
@@ -8,12 +9,12 @@ export const SelectLanguage = () => {
 
 
   return (
-    <>
-    <h3>Idioma</h3>
-    <select name="" id="">
-        <option value="" onClick={()=>i18n.changeLanguage("es")}>Español</option>
-        <option value="" onClick={()=>i18n.changeLanguage("en")}>English</option>
+    <div className='content_select_lenguage'>
+    <select className='select_lenguage' name="" id="">
+      <option defaultValue='Lenguage' selected disabled hidden>Idioma</option>
+        <option defaultValue="Español" onClick={()=>i18n.changeLanguage("es")}>Español</option>
+        <option defaultValue="English" onClick={()=>i18n.changeLanguage("en")}>English</option>
     </select>
-    </>
+    </div>
   )
 }
